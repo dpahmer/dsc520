a18ebddcb347d8dc8ae9f299c442af36051d1989
David Pahmer <pahmer@gmail.com>
2022-04-03 23:15:05 -0400

    Add files via upload

--- completed/assignment03/assignment_03_PahmerDavid.r
+++ completed/assignment03/assignment_03_PahmerDavid.r
@@ -16,37 +16,59 @@ heights_df <- read.csv("data/r4ds/heights.csv")
 ## Using `geom_point()` create three scatterplots for
 ## `height` vs. `earn`
 ggplot(heights_df, aes(x=height, y=earn)) + geom_point()
+ggsave("earn vs height.jpg")
+
 ## `age` vs. `earn`
 ggplot(heights_df, aes(y=earn, x=age)) + geom_point()
+ggsave("earn vs age.jpg")
+
 ## `ed` vs. `earn`
 ggplot(heights_df, aes(y=earn, x=ed)) + geom_point()
+ggsave("earn vs ed.jpg")
+
 
 ## Re-create the three scatterplots and add a regression trend line using
 ## the `geom_smooth()` function
 ## `height` vs. `earn`
 ggplot(heights_df, aes(x=height, y=earn)) + geom_point() + geom_smooth()
+ggsave("earn vs height and reg.jpg")
+
+
 ## `age` vs. `earn`
 ggplot(heights_df, aes(y=earn, x=age)) + geom_point() + geom_smooth()
+ggsave("earn vs age and reg.jpg")
+
 ## `ed` vs. `earn`
 ggplot(heights_df, aes(y=earn, x=ed)) + geom_point() + geom_smooth()
+ggsave("earn vs ed and reg.jpg")
+
 
 ## Create a scatterplot of `height`` vs. `earn`.  Use `sex` as the `col` (color) attribute
 ggplot(heights_df, aes(x=height, y=earn, col=sex)) + geom_point()
+ggsave("earn vs height and sex.jpg")
 
 ## Using `ggtitle()`, `xlab()`, and `ylab()` to add a title, x label, and y label to the previous plot
 ## Title: Height vs. Earnings
 ## X label: Height (Inches)
 ## Y Label: Earnings (Dollars)
-ggplot(heights_df, aes(x=height, y=earn, col=sex)) + geom_point() + ggtitle("Earnings vs. Height") + xlab("Height (inches)") + ylab("Earnings (Dollars)")
+ggplot(heights_df, aes(x=height, y=earn, col=sex)) + geom_point() + ggtitle("Earnings vs. Height") + 
+  xlab("Height (inches)") + ylab("Earnings (Dollars)")
+ggsave("earn vs height with labels.jpg")
+
 
 # https://ggplot2.tidyverse.org/reference/geom_histogram.html
 ## Create a histogram of the `earn` variable using `geom_histogram()`
 ggplot(heights_df, aes(earn)) + geom_histogram()
+ggsave("earn histogram.jpg")
 
 ## Create a histogram of the `earn` variable using `geom_histogram()`
 ## Use 10 bins
 ggplot(heights_df, aes(earn)) + geom_histogram(bins = 10)
+ggsave("earn histogram 10 bins.jpg")
+
 
 # https://ggplot2.tidyverse.org/reference/geom_density.html
 ## Create a kernel density plot of `earn` using `geom_density()`
 ggplot(heights_df, aes(earn)) + geom_density()
+ggsave("earn density.jpg")
+
